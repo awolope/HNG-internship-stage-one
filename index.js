2ab1ca306f13de0ed7900dda86e0b8968afd5b43
Hernnie <marvelouspeculiar@gmail.com>
2024-07-01 10:18:25 +0100

    started working on the project

---
+++ index.js
@@ -0,0 +1,21 @@
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let currentDateELement = document.querySelector("#currentdate");
+let currentDate = new Date();
+currentDateELement.innerHTML = formatDate(currentDate);
